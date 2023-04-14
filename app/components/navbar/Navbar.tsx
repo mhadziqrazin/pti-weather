034a3c8a3651d1f66fc28d4c907f328e786e2db4
mhadziq0403 <mhadziq0403@gmail.com>
2023-04-15 04:02:09 +0700

    init movie page

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,19 +1,34 @@
+import Link from "next/link"
 import Container from "../Container"
 
 const Navbar = () => {
   return (
     <nav className="py-4">
       <Container>
-        <div className="flex items-center gap-2">
-          <div className="text-pr font-bold animated">
-            <span className="text-blue-500">
-              PTI
-            </span>
-            &nbsp;
-            <span className="text-red-500">
-              Weather
-            </span>
-          </div>
+        <div className="flex items-center justify-between">
+          <Link href={"/"}>
+            <div className="text-pr font-bold animated">
+              <span className="text-blue-500">
+                PTI
+              </span>
+              &nbsp;
+              <span className="text-red-500">
+                Weather
+              </span>
+            </div>
+          </Link>
+          <Link href={"/movie"}>
+            <div className="text-pr font-bold animated">
+              <span className="text-blue-500">
+                PTI
+              </span>
+              &nbsp;
+              <span className="text-red-500">
+                Movies
+              </span>
+            </div>
+          </Link>
+
         </div>
       </Container>
     </nav>
