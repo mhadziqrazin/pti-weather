7a31a11c44ea514f724babf0205ddfbbab7af433
mhadziq0403 <mhadziq0403@gmail.com>
2023-04-16 06:27:38 +0700

    add image config

--- app/components/Movie.tsx
+++ app/components/Movie.tsx
@@ -38,75 +38,77 @@ const Movie: React.FC<Props> = (
       <img
         src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
         alt={`${movie.title}`}
-
       />
-      <div
-        className="
-          text-sc
-          font-medium
-          p-4
-          flex
-          place-content-between
-          gap-2
-          items-center
-        "
-      >
-        <p>{movie.title}</p>
-        <p>{movie.vote_average}</p>
-      </div>
-
-      <div
-        className="
-          p-4
-          flex
-          flex-col
-          gap-3
-          absolute
-          bottom-0
-          bg-gray-900
-          text-white
-          hover:drop-shadow-[0_0px_25px_rgba(0,0,0)]
-          rounded-xl
-          overflow-y-auto
-          max-h-full
-          translate-y-full
-          group-hover:translate-y-0
-          animated
-        "
-      >
-        <p className="font-bold text-sc">
-          Description:
-        </p>
-        <p
+      <div className="h-ful">
+        <div
           className="
             text-sc
+            font-medium
+            p-4
+            flex
+            place-content-between
+            gap-2
+            items-center
           "
         >
-          {movie.overview}
-        </p>
-        <div className="flex place-content-center">
-          <Button
-            type="button"
-            disabled={false}
-            className="
-            font-medium
-            text-sc
-          text-gray-900
-          bg-blue-300
-            border-2
-          border-blue-300
-            hover:bg-transparent hover:text-blue-300
-            px-4
-            py-2
-            rounded-lg
-            w-fit
+          <p>{movie.title}</p>
+          <p>{movie.vote_average}</p>
+        </div>
+
+        <div
+          className="
+            p-4
+            flex
+            flex-col
+            gap-3
+            absolute
+            bottom-0
+            bg-gray-900
+            text-white
+            group-hover:drop-shadow-[0_0px_25px_rgba(0,0,0)]
+            rounded-xl
+            overflow-y-auto
+            max-h-full
+            translate-y-full
+            group-hover:translate-y-0
             animated
           "
+        >
+          <p className="font-bold text-sc">
+            Description:
+          </p>
+          <p
+            className="
+            text-sc
+          "
           >
-            Add to Watchlist
-          </Button>
+            {movie.overview}
+          </p>
+          <div className="flex place-content-center">
+            <Button
+              type="button"
+              disabled={false}
+              className="
+                font-medium
+                text-sc
+              text-gray-900
+              bg-blue-300
+                border-2
+              border-blue-300
+                hover:bg-transparent hover:text-blue-300
+                px-4
+                py-2
+                rounded-lg
+                w-fit
+                animated
+              "
+            >
+              Add to Watchlist
+            </Button>
+          </div>
         </div>
       </div>
+
     </div>
 
   )
