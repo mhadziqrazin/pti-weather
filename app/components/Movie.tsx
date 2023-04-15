27da0d17e5c4f0a1f41e4eb8f21db8b34fc8a176
mhadziq0403 <mhadziq0403@gmail.com>
2023-04-16 02:33:14 +0700

    styling description and hover, tinggal logic watchlist

--- app/components/Movie.tsx
+++ app/components/Movie.tsx
@@ -1,3 +1,5 @@
+import Button from "./Button"
+
 export interface MovieProps {
   adult: boolean
   backdrop_path: string
@@ -25,31 +27,88 @@ const Movie: React.FC<Props> = (
   return (
     <div
       className="
-      bg-white
-      text-black
+      bg-gray-900
         max-w-[18rem]
         rounded-xl
-        rounded-t-2xl
+        relative
+        overflow-hidden
+        group
       "
     >
       <img
         src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
         alt={`${movie.title}`}
-        className="rounded-t-xl"
+
       />
-      <p>{movie.title}</p>
-      <p>{movie.vote_average}</p>
-      <p
+      <div
         className="
-          relative
-          h-0
-          overflow-hidden
-          hover:h-auto
+          text-sc
+          font-medium
+          p-4
+          flex
+          place-content-between
         "
       >
-        {movie.overview}
-      </p>
+        <p>{movie.title}</p>
+        <p>{movie.vote_average}</p>
+      </div>
+
+      <div
+        className="
+          p-4
+          flex
+          flex-col
+          gap-3
+          absolute
+          bottom-0
+          bg-gray-900
+          text-white
+          hover:drop-shadow-[0_0px_25px_rgba(0,0,0)]
+          rounded-t-xl
+          overflow-y-auto
+          max-h-full
+          translate-y-full
+          group-hover:translate-y-0
+          animated
+        "
+      >
+        <p className="font-bold text-sc">
+          Description:
+        </p>
+        <p
+          className="
+            text-sc
+          "
+        >
+          {movie.overview}
+        </p>
+        <div className="flex place-content-center">
+          <Button
+            type="button"
+            disabled={false}
+            className="
+            font-medium
+            text-sc
+          text-gray-900
+          bg-blue-300
+            border-2
+          border-blue-300
+            hover:bg-transparent hover:text-blue-300
+            px-4
+            py-2
+            rounded-lg
+            w-fit
+            animated
+          "
+          >
+            Add to Watchlist
+          </Button>
+        </div>
+
+      </div>
+
     </div>
+
   )
 }
 
